24fe8d641e6d8cfa26e5d11c968e2735c123bf85
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-08-19 02:20:53 +0300

    completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,7 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const SAVED_TIME = "videoplayer-current-time"  
-const onPlay = data => {const time = localStorage.setItem(SAVED_TIME, JSON.stringify(data.seconds));};
+const onPlay = data => {localStorage.setItem(SAVED_TIME, JSON.stringify(data.seconds));};
 player.on('timeupdate', throttle(onPlay, 1000));
 const currentTime = JSON.parse(localStorage.getItem(SAVED_TIME)) 
 if (currentTime) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const feedbackEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('input');
+const textEl = document.querySelector('textarea');
+
+const FEEDBACK = 'feedback-form-state';
+const storageData = JSON.parse(localStorage.getItem(FEEDBACK));
+
+const savedDataObj = {
+  email: storageData ? storageData.email : '',
+  message: storageData ? storageData.message : '',
+};
+
+feedbackEl.addEventListener('input', throttle(savedData, 500));
+feedbackEl.addEventListener('submit', onFormSubmit);
+
+autocomplete();
+
+function savedData(event) {
+  savedDataObj[event.target.name] = event.target.value;
+  localStorage.setItem(FEEDBACK, JSON.stringify(savedDataObj));
+}
+
+function autocomplete() {
+  if (storageData) {
+    inputEl.value = storageData.email;
+    textEl.value = storageData.message;
+  }
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const storageData = JSON.parse(localStorage.getItem(FEEDBACK));
+  if (storageData) {
+    console.log(storageData);
+  }
+  event.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK);
+}
