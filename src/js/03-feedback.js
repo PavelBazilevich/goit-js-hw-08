97a150aa2189211baa43ca13b45a2e5efc4e9225
PavelBazilevich <102550003+PavelBazilevich@users.noreply.github.com>
2022-08-19 16:42:58 +0300

    update hw

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,34 @@
 import throttle from 'lodash.throttle';
 
 const feedbackEl = document.querySelector('.feedback-form');
-const inputEl = document.querySelector('input');
-const textEl = document.querySelector('textarea');
-
 const FEEDBACK = 'feedback-form-state';
-const storageData = JSON.parse(localStorage.getItem(FEEDBACK));
 
-const savedDataObj = {
-  email: storageData ? storageData.email : '',
-  message: storageData ? storageData.message : '',
-};
+initForm();
 
-feedbackEl.addEventListener('input', throttle(savedData, 500));
-feedbackEl.addEventListener('submit', onFormSubmit);
-
-autocomplete();
-
-function savedData(event) {
-  savedDataObj[event.target.name] = event.target.value;
-  localStorage.setItem(FEEDBACK, JSON.stringify(savedDataObj));
-}
-
-function autocomplete() {
-  if (storageData) {
-    inputEl.value = storageData.email;
-    textEl.value = storageData.message;
-  }
-}
-
-function onFormSubmit(event) {
+feedbackEl.addEventListener('submit', event => {
   event.preventDefault();
-  const storageData = JSON.parse(localStorage.getItem(FEEDBACK));
-  if (storageData) {
-    console.log(storageData);
-  }
-  event.currentTarget.reset();
+  const savedDataObj = JSON.parse(localStorage.getItem(FEEDBACK));
+  console.log(savedDataObj);
   localStorage.removeItem(FEEDBACK);
+    event.currentTarget.reset();
+});
+
+feedbackEl.addEventListener(
+  'input',
+  throttle(event => {
+    let persistedStorage = localStorage.getItem(FEEDBACK);
+    persistedStorage = persistedStorage ? JSON.parse(persistedStorage) : {}
+    persistedStorage[event.target.name] = event.target.value;
+    localStorage.setItem(FEEDBACK, JSON.stringify(persistedStorage));
+  }, 500)
+);
+
+function initForm() {
+  let persistedStorage = localStorage.getItem(FEEDBACK);
+  if (persistedStorage) {
+    persistedStorage = JSON.parse(persistedStorage);
+    Object.entries(persistedStorage).forEach(([name, value]) => {
+      feedbackEl.elements[name].value = value;
+    })
+}
 }
